ab4116a6912dff7e9876c26866670f897c9edb08
Manisankarrr <manish.u2416@gmail.com>
2025-08-16 20:15:25 +0530

    Resume

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ text, className }) => {
   return (
     <a
-      href="/Dev Resume.pdf" // ✅ Correct path with encoded space
+      href="/ML.pdf" // ✅ Correct path with encoded space
       target="_blank"
       rel="noopener noreferrer"
       className={`${className ?? ""} cta-wrapper`}
