a986580b3b750c4755873aa4f1371303fde3032b
Manisankarrr <manish.u2416@gmail.com>
2025-09-06 12:18:33 +0530

    Resume Update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ text, className }) => {
   return (
     <a
-      href="/ML.pdf" // ✅ Correct path with encoded space
+      href="/ML3.pdf" // ✅ Correct path with encoded space
       target="_blank"
       rel="noopener noreferrer"
       className={`${className ?? ""} cta-wrapper`}
